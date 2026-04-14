c7b0ba5a533c181b1d7813670c232f50309cd326
James_Juguilon <jamesjuguilon123@gmail.com>
2026-04-14 21:08:57 +0800

    finish html, css, and javascript and try fix some of the codes

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // JS
+document.querySelectorAll('#nav-bar a').forEach(function(link) {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+
+const menuToggle = document.getElementById("menu-toggle");
+const navList = document.querySelector("#nav-bar ul");
+
+menuToggle.addEventListener("click", function() {
+  navList.classList.toggle("active");
+});
